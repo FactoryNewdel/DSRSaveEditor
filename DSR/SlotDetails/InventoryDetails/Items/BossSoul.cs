e8a26884935202176abbed0b9c2c8b751f50f5f7
Tim <tim.schnock2000@gmail.com>
2023-07-25 13:32:14 +0200

    Items

---
+++ DSR/SlotDetails/InventoryDetails/Items/BossSoul.cs
@@ -0,0 +1,23 @@
+﻿namespace DSR.SlotDetails.InventoryDetails.Items;
+
+public class BossSoul : CommonItem
+{
+    private UInt32 _soulAmount;
+    
+    public BossSoul(byte idSpace, uint id, uint amount, uint sorting, int index, bool enabled, uint durability, uint durabilityLoss) : base (idSpace, id, amount, sorting, index, enabled, durability, durabilityLoss)
+    {
+        
+    }
+
+    public BossSoul(ItemType type, byte idSpace, uint id, uint sorting) : base(type, idSpace, id, sorting)
+    {
+        
+    }
+
+    public BossSoul(BossSoul bossSoul) : base(bossSoul)
+    {
+        
+    }
+
+    public uint SoulAmount => _soulAmount;
+}
